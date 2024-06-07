b56f4f4cb4414dc87cfbde92027f37be0d577530
Al-Ferro Yudisthira Putra <alferroputra2003@gmail.com>
2024-06-07 18:19:14 +0700

    update database code

--- API_test/database_creation/nest_capstone.sql
+++ API_test/database_creation/nest_capstone.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 04, 2024 at 03:55 PM
+-- Generation Time: Jun 07, 2024 at 01:18 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -66,6 +66,20 @@ CREATE TABLE `assessment_category` (
   `category_name` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `assessment_category`
+--
+
+INSERT INTO `assessment_category` (`category_id`, `category_name`) VALUES
+(1, 'R'),
+(2, 'I'),
+(3, 'A'),
+(4, 'S'),
+(5, 'E'),
+(6, 'C'),
+(7, 'TIPI'),
+(8, 'VCL');
+
 -- --------------------------------------------------------
 
 --
@@ -78,6 +92,24 @@ CREATE TABLE `assessment_option` (
   `option_text` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `assessment_option`
+--
+
+INSERT INTO `assessment_option` (`option_id`, `option_code`, `option_text`) VALUES
+(1, 1, 'Tidak Suka'),
+(2, 2, 'Cukup Tidak Suka'),
+(3, 3, 'Netral'),
+(4, 4, 'Cukup Suka'),
+(5, 5, 'Suka'),
+(6, 1, 'Sangat tidak setuju'),
+(7, 2, 'Cukup tidak setuju'),
+(8, 3, 'Sedikit tidak setuju'),
+(9, 4, 'Tidak setuju dan tidak setuju'),
+(10, 5, 'Setuju sedikit'),
+(11, 6, 'Cukup Setuju'),
+(12, 7, 'Sangat Setuju');
+
 -- --------------------------------------------------------
 
 --
@@ -85,21 +117,128 @@ CREATE TABLE `assessment_option` (
 --
 
 CREATE TABLE `assessment_question` (
-  `question_id` int(11) NOT NULL,
+  `question_id` varchar(10) NOT NULL,
   `question_text` varchar(255) NOT NULL,
   `assessment_category_category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `assessment_question`
+--
+
+INSERT INTO `assessment_question` (`question_id`, `question_text`, `assessment_category_category_id`) VALUES
+('A2', 'Mengarahkan sebuah pertunjukan', 3),
+('A3', 'Merancang karya seni untuk majalah', 3),
+('A4', 'Menulis lagu', 3),
+('A5', 'Menulis buku atau drama', 3),
+('A6', 'Bermain alat musik', 3),
+('A8', 'Merancang set untuk drama', 3),
+('C1', 'Menghasilkan cek gaji bulanan untuk sebuah kantor', 6),
+('C2', 'Menghitung persediaan menggunakan komputer genggam', 6),
+('C3', 'Menggunakan program komputer untuk menghasilkan \ntagihan pelanggan', 6),
+('C5', 'Menghitung dan mencatat data statistik dan \nnumerik lainnya', 6),
+('C7', 'Menangani transaksi bank pelanggan', 6),
+('C8', 'Menjaga catatan pengiriman dan penerimaan', 6),
+('E1', 'Menjual waralaba restoran kepada individu', 5),
+('E3', 'Mengelola operasi hotel', 5),
+('E4', 'Mengoperasikan salon kecantikan atau pangkas rambut', 5),
+('E5', 'Mengelola departemen dalam perusahaan besar', 5),
+('E7', 'Menjual rumah', 5),
+('E8', 'Mengelola toko mainan', 5),
+('I1', 'Mempelajari struktur tubuh manusia', 2),
+('I2', 'Mempelajari perilaku hewan', 2),
+('I4', 'Mengembangkan pengobatan atau prosedur medis baru', 2),
+('I5', 'Melakukan penelitian biologi', 2),
+('I7', 'Bekerja di laboratorium biologi', 2),
+('I8', 'Membuat peta dasar laut', 2),
+('R1', 'Menguji kualitas bagian sebelum pengiriman', 1),
+('R2', 'Menyusun batu bata atau ubin', 1),
+('R4', 'Merakit bagian elektronik', 1),
+('R6', 'Memperbaiki keran yang rusak', 1),
+('R7', 'Merakit produk di pabrik', 1),
+('R8', 'Memasang lantai di rumah', 1),
+('S1', 'Memberi bimbingan karir kepada orang lain', 4),
+('S3', 'Membantu orang yang memiliki masalah dengan \nobat-obatan atau alkohol', 4),
+('S5', 'Membantu orang dengan masalah keluarga', 4),
+('S6', 'Mengawas aktivitas anak-anak di perkemahan', 4),
+('S7', 'Mengajar anak-anak membaca', 4),
+('S8', 'Membantu orang tua dengan aktivitas sehari-hari', 4),
+('TIPI1', 'Ekstrovert, antusias.', 7),
+('TIPI10', 'Konvensional, tidak kreatif.', 7),
+('TIPI2', 'Kritis, suka bertengkar.', 7),
+('TIPI3', 'Dapat diandalkan, disiplin diri.', 7),
+('TIPI4', 'Cemas, mudah marah.', 7),
+('TIPI5', 'Terbuka terhadap pengalaman baru, kompleks.', 7),
+('TIPI6', 'Pendiam, tenang.', 7),
+('TIPI7', 'Simpatik, hangat.', 7),
+('TIPI8', 'Tidak terorganisir, ceroboh.', 7),
+('TIPI9', 'Tenang, stabil secara emosional.', 7),
+('VCL1', 'boat', 8),
+('VCL10', 'decide', 8),
+('VCL11', 'pastiche', 8),
+('VCL12', 'verdid', 8),
+('VCL13', 'abysmal', 8),
+('VCL14', 'lucid', 8),
+('VCL15', 'betray', 8),
+('VCL2', 'incoherent', 8),
+('VCL3', 'pallid', 8),
+('VCL4', 'robot', 8),
+('VCL5', 'audible', 8),
+('VCL6', 'cuivocal', 8);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `assessment_question_assessment_option`
+-- Table structure for table `category_option`
 --
 
-CREATE TABLE `assessment_question_assessment_option` (
-  `assessment_question_question_id` int(11) NOT NULL,
-  `assessment_option_option_id` int(11) NOT NULL
+CREATE TABLE `category_option` (
+  `category_id` int(11) NOT NULL,
+  `option_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category_option`
+--
+
+INSERT INTO `category_option` (`category_id`, `option_id`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(2, 1),
+(2, 2),
+(2, 3),
+(2, 4),
+(2, 5),
+(3, 1),
+(3, 2),
+(3, 3),
+(3, 4),
+(3, 5),
+(4, 1),
+(4, 2),
+(4, 3),
+(4, 4),
+(4, 5),
+(5, 1),
+(5, 2),
+(5, 3),
+(5, 4),
+(5, 5),
+(6, 1),
+(6, 2),
+(6, 3),
+(6, 4),
+(6, 5),
+(7, 6),
+(7, 7),
+(7, 8),
+(7, 9),
+(7, 10),
+(7, 11),
+(7, 12);
 
 -- --------------------------------------------------------
 
@@ -186,20 +325,20 @@ CREATE TABLE `task` (
 --
 
 INSERT INTO `task` (`task_id`, `task_name`, `task_date`, `task_startTime`, `task_endTime`, `task_duration`, `task_focusTime`, `task_breakTime`, `task_priority`, `task_repeat`, `user_user_id`, `isCompleted`, `createdAt`) VALUES
-(6, 'Study Advanced Algebra', '2024-06-02', '07:00:00', '08:00:00', 60, 20, 10, 'medium', 1, 2, 1, '2024-05-31 14:48:24'),
 (9, 'Lunch Break', '2024-06-01', '12:30:00', '13:00:00', 30, 0, 30, 'Low', 0, 3, 1, '2024-06-01 07:58:32'),
-(10, 'Client Call', '2024-06-01', '14:00:00', '15:00:00', 60, 55, 5, 'High', 0, 2, 1, '2024-06-01 08:01:26'),
+(10, 'Client Call', '2024-06-01', '14:00:00', '15:00:00', 60, 55, 5, 'High', 0, 2, 1, '2024-06-05 07:34:47'),
 (11, 'Study Algebra', '2024-06-01', '07:00:00', '08:00:00', 60, 20, 10, 'low', 1, 2, 1, '2024-05-31 15:17:15'),
 (13, 'Study Science', '2024-06-01', '07:00:00', '08:00:00', 60, 20, 10, 'low', 1, 2, 0, '2024-05-31 22:16:35'),
 (14, 'Study Geo', '2024-06-01', '07:00:00', '08:00:00', 60, 20, 10, 'low', 1, 2, 1, '2024-06-01 07:53:41'),
-(15, 'Study Physics', '2024-06-01', '07:00:00', '08:00:00', 60, 20, 10, 'low', 1, 2, 1, '2024-05-31 15:17:19'),
-(16, 'Study Advanced history', '2024-06-02', '07:00:00', '08:00:00', 60, 20, 10, 'medium', 1, 2, 1, '2024-05-31 07:48:24'),
-(17, 'History Assigment', '2024-06-13', '12:30:00', '13:00:00', 30, 0, 30, 'Low', 0, 2, 0, '2024-05-31 07:48:24'),
+(15, 'Study Physics', '2024-06-01', '07:00:00', '08:00:00', 60, 20, 10, 'low', 1, 2, 0, '2024-06-05 07:33:18'),
+(16, 'Study Advanced history', '2024-06-02', '07:00:00', '08:00:00', 60, 20, 10, 'medium', 1, 2, 0, '2024-06-05 07:33:07'),
+(17, 'History Assigment', '2024-06-13', '12:30:00', '13:00:00', 30, 0, 30, 'Low', 0, 3, 0, '2024-05-31 07:48:24'),
 (18, 'Student Council Meeting', '2024-06-20', '14:00:00', '15:00:00', 60, 55, 5, 'High', 0, 2, 0, '2024-05-31 07:48:24'),
-(19, 'Java Language Exam', '2024-07-06', '07:00:00', '08:00:00', 60, 20, 10, 'low', 1, 2, 1, '2024-05-31 08:17:15'),
+(19, 'Java Language Exam', '2024-07-06', '07:00:00', '08:00:00', 60, 20, 10, 'low', 1, 2, 0, '2024-06-05 07:32:50'),
 (20, 'Study Java Advanced', '2024-07-11', '07:00:00', '08:00:00', 60, 20, 10, 'low', 1, 2, 0, '2024-05-31 15:16:35'),
 (21, 'Study German Language', '2024-08-01', '07:00:00', '08:00:00', 60, 20, 10, 'low', 1, 2, 0, '2024-05-31 15:16:50'),
-(22, 'Study Math Logaritmic', '2024-08-09', '07:00:00', '08:00:00', 60, 20, 10, 'low', 1, 2, 1, '2024-05-31 08:17:19');
+(22, 'Study Math Logaritmic', '2024-08-09', '07:00:00', '08:00:00', 60, 20, 10, 'low', 1, 2, 0, '2024-06-05 07:32:58'),
+(23, 'Study Physics', '2024-06-01', '07:00:00', '08:00:00', 60, 20, 10, 'low', 1, 2, 0, '2024-06-05 14:34:39');
 
 -- --------------------------------------------------------
 
@@ -245,20 +384,30 @@ INSERT INTO `university` (`university_id`, `university_name`, `university_locati
 
 CREATE TABLE `user_data` (
   `user_id` int(11) NOT NULL,
-  `user_name` varchar(40) NOT NULL,
-  `username` varchar(255) DEFAULT NULL
+  `username` varchar(255) DEFAULT NULL,
+  `user_fullname` varchar(255) DEFAULT NULL,
+  `user_birthDate` date DEFAULT NULL,
+  `user_school` varchar(100) DEFAULT NULL,
+  `user_gender` int(10) DEFAULT NULL,
+  `user_engNat` tinyint(1) DEFAULT NULL,
+  `user_religion` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_data`
 --
 
-INSERT INTO `user_data` (`user_id`, `user_name`, `username`) VALUES
-(2, 'John Doe', 'john'),
-(3, 'Jane Smith', 'jane'),
-(4, 'Alice Johnson', 'alice'),
-(5, 'Bob Brown', 'bob'),
-(6, 'Charlie Davis', 'charlie');
+INSERT INTO `user_data` (`user_id`, `username`, `user_fullname`, `user_birthDate`, `user_school`, `user_gender`, `user_engNat`, `user_religion`) VALUES
+(2, 'john', 'Sergio Putra', '1990-01-01', 'SMA Negeri 3 Malang', 1, 1, 'Islam'),
+(3, 'jane', 'Sergia Putri', '2003-01-01', 'SMA Negeri 3 Malang', 2, 1, 'Islam'),
+(4, 'alice', 'Alice Johnson Boulder', '1988-04-04', 'Sample High School', 0, 0, 'Buddhism'),
+(5, 'bob', 'Bob White Black', '1995-05-05', 'Example College', 1, 1, 'Atheism'),
+(6, 'charlie', 'Charlie Brown Green', '1985-03-03', 'Another High School', 1, 0, 'Hinduism'),
+(7, 'johndoe', 'John Doe', '1990-01-01', 'SMA Negeri 1', 1, 1, 'Agnostik'),
+(8, 'janesmith', 'Jane Smith', '1992-02-02', 'SMA Negeri 2', 0, 1, 'Islam'),
+(9, 'mikebrown', 'Mike Brown', '1985-03-03', 'SMA Negeri 3', 1, 0, 'Ateis'),
+(10, 'alicejohnson', 'Alice Johnson', '1988-04-04', ' SMA Negeri 4', 0, 0, 'Budha'),
+(11, 'bobwhite', 'Bob White', '1995-05-05', 'SMA Negeri 5', 1, 1, 'Hindu');
 
 -- --------------------------------------------------------
 
@@ -276,12 +425,12 @@ CREATE TABLE `user_major` (
 --
 
 INSERT INTO `user_major` (`user_user_id`, `major_major_id`) VALUES
-(2, 1),
 (2, 4),
 (3, 1),
 (4, 2),
 (5, 3),
-(6, 5);
+(6, 5),
+(7, 1);
 
 --
 -- Indexes for dumped tables
@@ -314,11 +463,11 @@ ALTER TABLE `assessment_question`
   ADD KEY `assessment_question_assessment_category` (`assessment_category_category_id`);
 
 --
--- Indexes for table `assessment_question_assessment_option`
+-- Indexes for table `category_option`
 --
-ALTER TABLE `assessment_question_assessment_option`
-  ADD PRIMARY KEY (`assessment_question_question_id`,`assessment_option_option_id`),
-  ADD KEY `assessment_question_assessment_option_assessment_option` (`assessment_option_option_id`);
+ALTER TABLE `category_option`
+  ADD PRIMARY KEY (`category_id`,`option_id`),
+  ADD KEY `option_id` (`option_id`);
 
 --
 -- Indexes for table `major`
@@ -386,7 +535,7 @@ ALTER TABLE `potential_jobs`
 -- AUTO_INCREMENT for table `task`
 --
 ALTER TABLE `task`
-  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `university`
@@ -398,7 +547,7 @@ ALTER TABLE `university`
 -- AUTO_INCREMENT for table `user_data`
 --
 ALTER TABLE `user_data`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Constraints for dumped tables
@@ -417,11 +566,11 @@ ALTER TABLE `assessment_question`
   ADD CONSTRAINT `assessment_question_assessment_category` FOREIGN KEY (`assessment_category_category_id`) REFERENCES `assessment_category` (`category_id`);
 
 --
--- Constraints for table `assessment_question_assessment_option`
+-- Constraints for table `category_option`
 --
-ALTER TABLE `assessment_question_assessment_option`
-  ADD CONSTRAINT `assessment_question_assessment_option_assessment_option` FOREIGN KEY (`assessment_option_option_id`) REFERENCES `assessment_option` (`option_id`),
-  ADD CONSTRAINT `assessment_question_assessment_option_assessment_question` FOREIGN KEY (`assessment_question_question_id`) REFERENCES `assessment_question` (`question_id`);
+ALTER TABLE `category_option`
+  ADD CONSTRAINT `category_option_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `assessment_category` (`category_id`),
+  ADD CONSTRAINT `category_option_ibfk_2` FOREIGN KEY (`option_id`) REFERENCES `assessment_option` (`option_id`);
 
 --
 -- Constraints for table `potential_jobs`
